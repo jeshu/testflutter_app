9c8f2c377a102aaa32083fec984deb0df09593de
Jeshu Brij <jeshu911@gmail.com>
2021-03-08 20:06:51 +0530

    Code updated for Creating widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,8 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey[900],
-          title: Text('I Am Rich'),
-        ),
-        body: Center(
-          child: Image(
-            image: AssetImage('images/diamond.png'),
-          ),
-        ),
+        backgroundColor: Colors.teal,
+        body: Container(),
       ),
     ),
   );
